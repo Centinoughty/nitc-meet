9cc6e286e353a985470462c6847dcb533abefa98
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-01-26 09:08:40 +0530

    authentication using next auth

--- frontend/src/app/api/auth/[...nextauth]/route.js
+++ frontend/src/app/api/auth/[...nextauth]/route.js
@@ -6,18 +6,23 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/google`,
+        },
+      },
     }),
   ],
   secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
-  callbacks: {
-    async signIn({ profile }) {
-      if (profile.email.endsWith("@nitc.ac.in")) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-  },
+  // callbacks: {
+  //   async signIn({ profile }) {
+  //     if (profile.email.endsWith("@nitc.ac.in")) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   },
+  // },
 });
 
 export { handler as GET, handler as POST };
